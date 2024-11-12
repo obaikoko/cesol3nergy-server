fe5d31c9c21f56e048dd4dd601aeb1a5920e750e
obaikoko <jesseobinna7@gmail.com>
2024-11-12 10:18:28 +0100

    updated createOrder

--- routes/orderRoutes.js
+++ routes/orderRoutes.js
@@ -1,16 +1,16 @@
 import express from 'express';
 import {
-  // addOrderItems,
+  addOrderItems,
   getMyOrders,
   getOrderById,
   updateOrderToPaid,
   updateOrderToDelivered,
-  // getOrders,
+  getOrders,
 } from '../controllers/orderController.js';
 import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
-// router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
+router.route('/').post(protect, addOrderItems).get(protect, admin, getOrders);
 router.route('/mine').get(protect, getMyOrders);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
